a7d552fcbfa1f7196b5b74700a4d77ecbca401bd
burlizzi <burlizzi@gmail.com>
2023-04-13 15:36:37 +0200

    Update test.php

--- test.php
+++ test.php
@@ -6,3 +6,4 @@
  <?php echo '<p>Hello World</p>'; ?> 
  </body>
 </html>
+[ `date '+%s'` -gt  $((`date -d "$(date +%Y)0331" '+%s'` - `date '+%u'` * 24 *3600)) ] && [ `date '+%s'` -lt  $((`date -d "$(date +%Y)1031" '+%s'` - `date '+%u'` * 24 *3600)) ] && echo ciao
